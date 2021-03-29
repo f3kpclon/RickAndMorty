7b4db994d4fb37c8a7306171f9e71c5dd1cb9e21
Felix Sotelo <felix.sotelo@wom.cl>
2021-03-29 10:14:43 -0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Felix Alexander Sotelo Quezada on 29-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
